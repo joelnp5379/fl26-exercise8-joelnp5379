efaacb5034269f739bf174eb0c76ccd2a46e1bcc
Joel Phillips <joelnp5379@vt.edu>
2026-09-16 20:07:37 -0400

    Completed Exercise 8

---
+++ label_printer.cpp
@@ -0,0 +1,6 @@
+#include "label_printer.h"
+#include <QDebug>
+LabelPrinter::LabelPrinter(const char * labelstr): QLabel(labelstr) {};
+void LabelPrinter::keyPressEvent(QKeyEvent *ev){
+    qDebug() << "Hello World!";
+}
